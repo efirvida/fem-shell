cb920f86b9edfa7066775704887e52fce69b0cf9
Eduardo Firvida <efirvida@gmail.com>
2026-01-10 11:47:47 -0300

    feat(elements): export MITC3 and MITC3Composite from elements module
    Update __init__.py to include new triangular elements:
    - MITC3: base triangular shell element
    - MITC3Composite: laminated composite triangular element

--- src/fem_shell/elements/__init__.py
+++ src/fem_shell/elements/__init__.py
@@ -1,6 +1,8 @@
 from .elements import ElementFactory, ElementFamily, FemElement
 from .MITC4 import MITC4
 from .MITC4_composite import MITC4Composite
+from .MITC3 import MITC3
+from .MITC3_composite import MITC3Composite
 
 __all__ = [
     "ElementFactory",
@@ -8,4 +10,6 @@ __all__ = [
     "FemElement",
     "MITC4",
     "MITC4Composite",
+    "MITC3",
+    "MITC3Composite",
 ]
